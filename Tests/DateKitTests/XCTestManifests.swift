e634688a152555c47b9a48f62d4dcab67b900f10
Jacob Sikorski <jacob.sikorski@gmail.com>
2019-01-05 18:54:34 -0500

    Remove `allTests`

--- Tests/DateKitTests/XCTestManifests.swift
+++ Tests/DateKitTests/XCTestManifests.swift
@@ -3,10 +3,6 @@ import XCTest
 #if !os(macOS)
 public func allTests() -> [XCTestCaseEntry] {
     return [
-        testCase(DayTests.allTests),
-        testCase(TimeTests.allTests),
-        testCase(DayTimeTests.allTests),
-        testCase(DurationTests.allTests)
     ]
 }
 #endif
